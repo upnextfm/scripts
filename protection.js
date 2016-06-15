9fedbe7756b2df792549897da1bce0f8c2e0ccb0
headzoo <sean@headzoo.io>
2016-06-15 02:52:33 -0400

    Protection script updated

--- protection.js
+++ protection.js
@@ -1,12 +1,14 @@
 /**
  * Script: Protection
- * Version: 1.6
+ * Version: 1.7
  *
  * Provides protection against trolls and other nasty users.
  * 
  * Protection settings are configured by clicking Options -> Protection.
  * Click a user's name in the users list and click "Protection On" to 
  * guard against that user.
+ * 
+ * REFRESH THE PAGE AFTER INSTALLING.
  */
 (function() {
     var settings = $api.getStorage("protection-settings", {
@@ -15,99 +17,10 @@
         no_upper_case: true,
         blocked_phrases: ""
     });
-    var trolls   = $api.getStorage("protection-trolls", []);
-    var no_queue = $api.getStorage("protection-no-queue", []);
+    var trolls    = $api.getStorage("protection-trolls", []);
+    var no_queue  = $api.getStorage("protection-no-queue", []);
+    var img_regex = /<img.+?src=[\"'](.+?)[\"'].*?>/g;
     var blocked_phrases_split = [];
-    
-    // Filter messages from users that have been put in troll prison.
-    $api.on("receive", function(e, data) {
-        if (trolls.indexOf(data.username.toLowerCase()) !== -1) {
-            data.meta.no_emotes = settings.no_emotes;
-            if (settings.no_upper_case) {
-                data.msg = data.msg.toLowerCase();
-            }
-            
-            if (settings.no_images) {
-                var regex = /<img.+?src=[\"'](.+?)[\"'].*?>/g;
-                var match = regex.exec(data.msg);
-                while(match != null) {
-                    if (match[1].indexOf("/proxy/image?u=") === 0) {
-                        match[1] = match[1].replace("/proxy/image?u=", "")
-                    }
-                    
-                    var replacement = '<a href="' + match[1] + '" target="_blank">' + match[1] + '</a>';
-                    data.msg = data.msg.replace(match[0], replacement);
-                    match = regex.exec(data.msg);
-                }
-            }
-        }
-        
-        $api.each(blocked_phrases_split, function(phrase) {
-            if (data.msg.toLowerCase().indexOf(phrase) != -1) {
-                e.cancel();
-                return null;
-            }
-        });
-    });
-    
-    // Stop trolls from queuing songs.
-    $api.on("queue", function(e, data) {
-        if (no_queue.indexOf(data.item.queueby) != -1) {
-            setTimeout(function() {
-                $api.dequeueByName(data.item.queueby);
-            }, 1000);
-        }
-    });
-    
-    // Save our settings when the rest of the user options are saved.
-    $api.on("user_options_save", function() {
-        updateSettings();
-        $api.setStorage("protection-settings", settings);
-    });
-    
-    // Add buttons to each profile menu which turns protection on/off.
-    $api.on("profile_menu", function(e, menu) {
-        var name      = menu.data("name").toLowerCase();
-        var btn_group = menu.find(".btn-group-vertical:first");
-        var btn = $("<button/>")
-            .addClass("btn btn-xs btn-default btn-stop-trolling")
-            .appendTo(btn_group);
-        
-        // Add a button to user profile menus to turn trolling protection on and off.
-        btn.text(trolls.indexOf(name) == -1 ? "Protection On" : "Protection Off")
-            .click(function () {
-                var index = trolls.indexOf(name);
-                if (index == -1) {
-                    trolls.push(name);
-                    btn.text("Protection Off");
-                } else {
-                    trolls.splice(index, 1);
-                    btn.text("Protection On");
-                }
-                
-                $api.setStorage("protection-trolls", trolls);
-            });
-        
-        // Gives mods a button to stop the user from adding to the queue.
-        if ($user.rank >= 2) {
-            var btnq = $("<button/>")
-                .addClass("btn btn-xs btn-default btn-stop-trolling-playlist")
-                .appendTo(btn_group);
-            btnq.text(no_queue.indexOf(name) == -1 ? "No Queue On" : "No Queue Off")
-                .click(function() {
-                    var index = no_queue.indexOf(name);
-                    if (index == -1) {
-                        no_queue.push(name);
-                        btnq.text("No Queue Off");
-                    } else {
-                        no_queue.splice(index, 1);
-                        btnq.text("No Queue On");
-                    }
-                    
-                    $api.setStorage("protection-no-queue", no_queue);
-                });
-        }
-    });
     
     // Adds a "Protection" tab to the user options where the protection
     // settings can be configured.
@@ -223,6 +136,113 @@
             blocked_phrases_split = [];
         }
     };
+    
+    // Converts <img/> tags into plain text.
+    var filterImages = function(msg) {
+        var match = img_regex.exec(msg);
+        while(match != null) {
+            if (match[1].indexOf("/proxy/image?u=") === 0) {
+                match[1] = match[1].replace("/proxy/image?u=", "")
+            }
+        
+            var replacement = '<a href="' + match[1] + '" target="_blank">' + match[1] + '</a>';
+            msg   = msg.replace(match[0], replacement);
+            match = img_regex.exec(msg);
+        }
+        
+        return msg;
+    };
+    
+    // Returns whether the message contains a blocked phrase.
+    var hasBlockedPhrase = function(msg) {
+        var found = false;
+        msg = msg.toLowerCase();
+        $api.each(blocked_phrases_split, function(phrase) {
+            if (msg.indexOf(phrase) != -1) {
+                found = true;
+                return null;
+            }
+        });
+        
+        return found;
+    };
+    
+    // Filter messages from users that have been put in troll prison.
+    $api.on("receive", function(e, data) {
+        if (hasBlockedPhrase(data.msg)) {
+            return e.cancel();
+        }
+        
+        if (trolls.indexOf(data.username.toLowerCase()) !== -1) {
+            data.meta.no_emotes = settings.no_emotes;
+            if (settings.no_upper_case) {
+                data.msg = data.msg.toLowerCase();
+            }
+            if (settings.no_images) {
+                console.log(data.msg);
+                data.msg = filterImages(data.msg);
+            }
+        }
+    });
+    
+    // Stop trolls from queuing songs.
+    $api.on("queue", function(e, data) {
+        if (no_queue.indexOf(data.item.queueby) != -1) {
+            setTimeout(function() {
+                $api.dequeueByName(data.item.queueby);
+            }, 1000);
+        }
+    });
+    
+    // Save our settings when the rest of the user options are saved.
+    $api.on("user_options_save", function() {
+        updateSettings();
+        $api.setStorage("protection-settings", settings);
+    });
+    
+    // Add buttons to each profile menu which turns protection on/off.
+    $api.on("profile_menu", function(e, menu) {
+        var name      = menu.data("name").toLowerCase();
+        var btn_group = menu.find(".btn-group-vertical:first");
+        var btn = $("<button/>")
+            .addClass("btn btn-xs btn-default btn-stop-trolling")
+            .appendTo(btn_group);
+        
+        // Add a button to user profile menus to turn trolling protection on and off.
+        btn.text(trolls.indexOf(name) == -1 ? "Protection On" : "Protection Off")
+            .click(function () {
+                var index = trolls.indexOf(name);
+                if (index == -1) {
+                    trolls.push(name);
+                    btn.text("Protection Off");
+                } else {
+                    trolls.splice(index, 1);
+                    btn.text("Protection On");
+                }
+                
+                $api.setStorage("protection-trolls", trolls);
+            });
+        
+        // Gives mods a button to stop the user from adding to the queue.
+        if ($user.rank >= 2) {
+            var btnq = $("<button/>")
+                .addClass("btn btn-xs btn-default btn-stop-trolling-playlist")
+                .appendTo(btn_group);
+            btnq.text(no_queue.indexOf(name) == -1 ? "No Queue On" : "No Queue Off")
+                .click(function() {
+                    var index = no_queue.indexOf(name);
+                    if (index == -1) {
+                        no_queue.push(name);
+                        btnq.text("No Queue Off");
+                    } else {
+                        no_queue.splice(index, 1);
+                        btnq.text("No Queue On");
+                    }
+                    
+                    $api.setStorage("protection-no-queue", no_queue);
+                });
+        }
+    });
     
     removeOptions();
     addOptions();
