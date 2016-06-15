de44daabdc4b39e38c5467e059b3fa1f27f446a1
headzoo <sean@headzoo.io>
2016-06-15 00:24:41 -0400

    Bot script updated

--- hide-bots.js
+++ hide-bots.js
@@ -1,6 +1,6 @@
 /**
  * Script: Hide Bots
- * Version: 1.1
+ * Version: 1.2
  * Author: headzoo
  * 
  * Hides site bots from the users list. Refresh your page after
@@ -14,22 +14,25 @@
     var bots = [
         "PieNudesBot"
     ];
+    var hidden = [];
     
     $api.on("user_count", function(e, data) {
-        data.count = data.count - bots.length;
+        data.count = data.count - hidden.length;
     });
     
     $api.on("user_join", function(e, data) {
-        if (data.meta.is_bot) {
-            bots.push(data.name);
+        if (data.meta.is_bot || bots.indexOf(data.name) != -1) {
+            $api.ignore(data.name);
+            hidden.push(data.name);
             e.cancel();
         }
     });
     
     $api.on("user_leave", function(e, data) {
-        var index = bots.indexOf(data.name);
+        var index = hidden.indexOf(data.name);
         if (index != -1) {
-            bots.splice(index, 1);
+            $api.unignore(data.name);
+            hidden.splice(index, 1);
         }
     });
     
