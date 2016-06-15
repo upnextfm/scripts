88eadf3993996c9521114ab76bdae071774bbe49
headzoo <sean@headzoo.io>
2016-06-15 02:02:44 -0400

    Protection script now has user options

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ Adds a new user option under the Options menu, where users can specify a comma s
 ### [Lucky](https://raw.githubusercontent.com/upnextfm/scripts/master/lucky.js)
 Creates a `/lucky` command, which searches YouTube using the query following the command, and queues the first video found.
 
+### [Protection](https://raw.githubusercontent.com/upnextfm/scripts/master/protection.js)
+Provides protection against trolls and other nasty users.
+
 ### [React](https://raw.githubusercontent.com/upnextfm/scripts/master/react.js)
 Creates a `/react` command, which displays a random image from replygif.net based on your search term.
-
-### [Troll Protection](https://raw.githubusercontent.com/upnextfm/scripts/master/troll-protection.js)
-Prevents trolls from showing images and emotes in the channel, and converts their messages to lower case letters.
---
+++ protection.js
@@ -0,0 +1,229 @@
+/**
+ * Script: Protection
+ * Version: 1.6
+ *
+ * Provides protection against trolls and other nasty users.
+ * 
+ * Protection settings are configured by clicking Options -> Protection.
+ * Click a user's name in the users list and click "Protection On" to 
+ * guard against that user.
+ */
+(function() {
+    var settings = $api.getStorage("protection-settings", {
+        no_images:     true,
+        no_emotes:     true,
+        no_upper_case: true,
+        blocked_phrases: ""
+    });
+    var trolls   = $api.getStorage("protection-trolls", []);
+    var no_queue = $api.getStorage("protection-no-queue", []);
+    var blocked_phrases_split = [];
+    
+    // Filter messages from users that have been put in troll prison.
+    $api.on("receive", function(e, data) {
+        if (trolls.indexOf(data.username.toLowerCase()) !== -1) {
+            data.meta.no_emotes = settings.no_emotes;
+            if (settings.no_upper_case) {
+                data.msg = data.msg.toLowerCase();
+            }
+            
+            if (settings.no_images) {
+                var regex = /<img src="([^"]+)".*\/>/g;
+                var match = regex.exec(data.msg);
+                while(match != null) {
+                    if (match[1].indexOf("/proxy/image?u=") === 0) {
+                        match[1] = match[1].replace("/proxy/image?u=", "")
+                    }
+                    data.msg = data.msg.replace(match[0], match[1]);
+                    match = regex.exec(data.msg);
+                }
+            }
+        }
+        
+        $api.each(blocked_phrases_split, function(phrase) {
+            if (data.msg.toLowerCase().indexOf(phrase) != -1) {
+                e.cancel();
+                return null;
+            }
+        });
+    });
+    
+    // Stop trolls from queuing songs.
+    $api.on("queue", function(e, data) {
+        if (no_queue.indexOf(data.item.queueby) != -1) {
+            setTimeout(function() {
+                $api.dequeueByName(data.item.queueby);
+            }, 1000);
+        }
+    });
+    
+    // Save our settings when the rest of the user options are saved.
+    $api.on("user_options_save", function() {
+        updateSettings();
+        $api.setStorage("protection-settings", settings);
+    });
+    
+    // Add buttons to each profile menu which turns protection on/off.
+    $api.on("profile_menu", function(e, menu) {
+        var name      = menu.data("name").toLowerCase();
+        var btn_group = menu.find(".btn-group-vertical:first");
+        var btn = $("<button/>")
+            .addClass("btn btn-xs btn-default btn-stop-trolling")
+            .appendTo(btn_group);
+        
+        // Add a button to user profile menus to turn trolling protection on and off.
+        btn.text(trolls.indexOf(name) == -1 ? "Protection On" : "Protection Off")
+            .click(function () {
+                var index = trolls.indexOf(name);
+                if (index == -1) {
+                    trolls.push(name);
+                    btn.text("Protection Off");
+                } else {
+                    trolls.splice(index, 1);
+                    btn.text("Protection On");
+                }
+                
+                $api.setStorage("protection-trolls", trolls);
+            });
+        
+        // Gives mods a button to stop the user from adding to the queue.
+        if ($user.rank >= 2) {
+            var btnq = $("<button/>")
+                .addClass("btn btn-xs btn-default btn-stop-trolling-playlist")
+                .appendTo(btn_group);
+            btnq.text(no_queue.indexOf(name) == -1 ? "No Queue On" : "No Queue Off")
+                .click(function() {
+                    var index = no_queue.indexOf(name);
+                    if (index == -1) {
+                        no_queue.push(name);
+                        btnq.text("No Queue Off");
+                    } else {
+                        no_queue.splice(index, 1);
+                        btnq.text("No Queue On");
+                    }
+                    
+                    $api.setStorage("protection-no-queue", no_queue);
+                });
+        }
+    });
+    
+    // Adds a "Protection" tab to the user options where the protection
+    // settings can be configured.
+    var addOptions = function() {
+        var options = $("#useroptions");
+        var header  = options.find(".modal-header");
+        var tabs    = header.find(".nav-tabs");
+        var panes   = options.find(".modal-body > .tab-content");
+        
+        var tab = $('<li/>');
+        tab.attr("id", "us-protection-tab");
+        tabs.append(tab);
+        
+        var anchor = $('<a/>');
+        anchor.attr("href", "#us-protection-pane");
+        anchor.attr("data-toggle", "tab");
+        anchor.text("Protection");
+        tab.append(anchor);
+        
+        var pane = $('<div/>');
+        pane.addClass("tab-pane");
+        pane.attr("id", "us-protection-pane");
+        panes.append(pane);
+        
+        var form = $('<form/>');
+        form.addClass("form-horizontal");
+        pane.append(form);
+        
+        form.append($('<h5>Troll Settings</h5>'));
+        $(
+            '<div class="form-group">' +
+                '<div class="col-sm-8 col-sm-offset-4">' +
+                    '<div class="checkbox">' +
+                        '<label for="us-protection-settings-no-images">' +
+                            '<input type="checkbox" id="us-protection-settings-no-images" />' +
+                            'Do not show inline images from trolls.' +
+                        '</label>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+        ).appendTo(form);
+    
+        $(
+            '<div class="form-group">' +
+                '<div class="col-sm-8 col-sm-offset-4">' +
+                    '<div class="checkbox">' +
+                        '<label for="us-protection-settings-no-emotes">' +
+                            '<input type="checkbox" id="us-protection-settings-no-emotes" />' +
+                            'Do not show emotes from trolls.' +
+                        '</label>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+        ).appendTo(form);
+    
+        $(
+            '<div class="form-group">' +
+                '<div class="col-sm-8 col-sm-offset-4">' +
+                    '<div class="checkbox">' +
+                        '<label for="us-protection-settings-no-upper-case">' +
+                            '<input type="checkbox" id="us-protection-settings-no-upper-case" />' +
+                            'Do not show UPPER CASE text from trolls.' +
+                        '</label>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+        ).appendTo(form);
+    
+        form.append($('<h5>General Settings</h5>'));
+        $(
+            '<div class="form-group">' +
+                '<label class="control-label col-sm-4" for="us-protection-settings-blocked-phrases">' +
+                    'Block messages containing' +
+                '</label>' +
+                '<div class="col-sm-8">' +
+                    '<input type="text" class="form-control" id="us-protection-settings-blocked-phrases"/>' +
+                    '<p class="text-muted">Comma separated list of phrases, e.g. "hamburgers, taco bell, hate".</p>' +
+                '</div>' +
+            '</div>'
+        ).appendTo(form);
+    };
+    
+    // Removes the "Protection" tab from the user options.
+    var removeOptions = function() {
+        $("#us-protection-tab").remove();
+        $("#us-protection-pane").remove();
+    };
+    
+    // Updates the local 'settings' object to the form values.
+    var updateSettings = function() {
+        settings.no_images       = $("#us-protection-settings-no-images").is(":checked");
+        settings.no_emotes       = $("#us-protection-settings-no-emotes").is(":checked");
+        settings.no_upper_case   = $("#us-protection-settings-no-upper-case").is(":checked");
+        settings.blocked_phrases = $("#us-protection-settings-blocked-phrases").val().trim();
+        splitBlockedPhrases();
+    };
+    
+    // Updates the form values from the local 'settings' object.
+    var updateForm = function() {
+        $("#us-protection-settings-no-images").prop("checked", settings.no_images);
+        $("#us-protection-settings-no-emotes").prop("checked", settings.no_emotes);
+        $("#us-protection-settings-no-upper-case").prop("checked", settings.no_upper_case);
+        $("#us-protection-settings-blocked-phrases").val(settings.blocked_phrases);
+    };
+    
+    // Splits the blocked phrases.
+    var splitBlockedPhrases = function() {
+        if (settings.blocked_phrases) {
+            blocked_phrases_split =
+                settings.blocked_phrases.toLowerCase().split(",")
+                    .map(Function.prototype.call, String.prototype.trim);
+        } else {
+            blocked_phrases_split = [];
+        }
+    };
+    
+    removeOptions();
+    addOptions();
+    updateForm();
+    splitBlockedPhrases();
+})();
