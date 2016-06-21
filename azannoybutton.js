6d254baf6d592cfdd2d142c0801780d8110fcaaa
upnextfm <sean@upnext.fm>
2016-06-21 14:11:28 -0400

    azannoybutton.js updated

--- azannoybutton.js
+++ azannoybutton.js
@@ -1,6 +1,6 @@
 /**
  * Script: Az Irritation Button
- * Version: 1.A
+ * Version: 1.1
  * Adds a button that sends a message to a bot
  * bot then adds to a count of irritations per username
  * super basic
@@ -8,7 +8,7 @@
 var azadd = function() {
 	var az= $('<button class="btn btn-default btn-sm" id="az-button">Az Just Annoyed Me!</button>');
 	az.on("click", function() {
-		$api.send("/w az_bot [azannoy]")
+		$api.command("az_bot", "azannoy")
 	});
 	$("#az-button").remove();
 	$(".chatbuttons > .btn-group").append(az);
