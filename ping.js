6da961b8bc131fcb1bcd664616d24388d2f0aa5f
upnextfm <sean@upnext.fm>
2016-06-21 21:25:59 -0400

    ping.js updated

--- ping.js
+++ ping.js
@@ -1,6 +1,6 @@
 /**
  * Name: Ping
- * Version: 1.0
+ * Version: 1.1
  * Author: headzoo
  * 
  * Displays your server ping time in the top right hand corner of the page.
@@ -10,21 +10,30 @@
     var ping_time = $('<div id="ping-time-script" style="position: absolute; right: 16px; top: 52px;font-family: monospace;font-size:10px;">0ms</div>');
     $("#main").append(ping_time);
     
-    var START_TIME = null;
+    var timer      = null;
+    var start_time = null;
     var sendPing = function() {
-        START_TIME = Date.now();
+        start_time = Date.now();
         socket.emit("chatPing", {});
     };
     
     var handlePong = function() {
-        var elapsed = (Date.now()) - START_TIME;
-        START_TIME = null;
-        ping_time.text(elapsed + "ms");
+        if (START_TIME !== null) {
+            var elapsed = (Date.now()) - start_time;
+            start_time  = null;
+            ping_time.text(elapsed + "ms");
+        }
     };
     
     socket.on("chatPong", handlePong);
     sendPing();
-    setInterval(function() {
+    timer = setInterval(function() {
         sendPing();
     }, 30000);
+    
+    $api.on("reloading", function() {
+        if (timer) {
+            clearInterval(timer);
+        }
+    });
 })();
