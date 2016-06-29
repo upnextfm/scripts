9377aa3ee99f3e5b53a2107975ab1c0da9306dcc
upnextfm <sean@upnext.fm>
2016-06-29 01:48:36 -0400

    hide-bots.js updated to api 1.4.1

--- script/hide-bots.js
+++ script/hide-bots.js
@@ -1,6 +1,6 @@
 /**
  * Name: Hide Bots
- * Version: 1.2
+ * Version: 1.3
  * Author: headzoo
  * 
  * Hides site bots from the users list. Refresh your page after
@@ -16,27 +16,27 @@
     ];
     var hidden = [];
     
-    $api.on("user_count", function(e, data) {
+    $chat.on("user_count", function(e, data) {
         data.count = data.count - hidden.length;
     });
     
-    $api.on("user_join", function(e, data) {
+    $chat.on("user_join", function(e, data) {
         if (data.meta.is_bot || bots.indexOf(data.name) != -1) {
-            $api.ignore(data.name);
+            $chat.ignore(data.name);
             hidden.push(data.name);
             e.cancel();
         }
     });
     
-    $api.on("user_leave", function(e, data) {
+    $chat.on("user_leave", function(e, data) {
         var index = hidden.indexOf(data.name);
         if (index != -1) {
-            $api.unignore(data.name);
+            $chat.unignore(data.name);
             hidden.splice(index, 1);
         }
     });
     
-    $api.on("loaded", function() {
+    $chat.on("loaded", function() {
         $(".userlist_item_is_bot").remove();
         $each(bots, function(bot) {
             $(".userlist_item_" + bot).remove();
