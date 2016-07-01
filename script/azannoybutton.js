5643eedbc540e9eb6323fcf115f2083f9d1ddd61
az4521 <cheeze.sprinkels@gmail.com>
2016-07-01 18:25:27 -0400

    use the new API thing

--- script/azannoybutton.js
+++ script/azannoybutton.js
@@ -1,19 +1,19 @@
 /**
  * Name: Az Irritation Button
- * Version: 1.C
+ * Version: 1.D - API Chage Fix Edition Why Change It So much Headz
  * Adds a button that sends a message to a bot
  * bot then adds to a count of irritations per username
  * The bot replies with commands, which are then seen as a toast
  */
-$api.on("command", function(e,data){
+$chat.on("command", function(e,data){
     if(data.from=="az_bot"){
-        $api.toast(data.data);
+        $chat.toast(data.data);
     }
 });
 var azadd = function() {
 	var az= $('<button class="btn btn-default btn-sm" id="az-button">Az Just Annoyed Me!</button>');
 	az.on("click", function() {
-		$api.command("az_bot", "azannoy")
+		$chat.command("az_bot", "azannoy")
 	});
 	$("#az-button").remove();
 	$(".chatbuttons > .btn-group").append(az);
