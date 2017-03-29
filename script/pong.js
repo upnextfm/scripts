384ede18de41c7bdeeb8665e9331a15071d01c8c
az4521 <cheeze.sprinkels@gmail.com>
2017-03-28 22:59:34 -0400

    Add multiplayer pong script
    pl0x add for anniversary of site

---
+++ script/pong.js
@@ -0,0 +1,21 @@
+/**
+ * Name: Peer-To-Peer Pong
+ * Version: 1.A
+ * Author: az4521
+ * 
+ * I FINALLY GOT IT WORKING SLIGHTLY.
+ * 
+ * basically lets you start a pong game
+ * with anyone else who has the script.
+ *
+ * commands:
+ *     /pong [username]  - request to start a pong game with another user
+ *     /pong accept      - accept a request to start a pong game
+ *     /pong decline     - decline a request to start a pong game
+ *
+ * bugs:
+ *     something with collision detection on client-side paddle. makes everything register as a collision.
+ *     lags a bunch with high ping connections
+ *     no way to end the game without refreshing the page
+ *     uses enough CPU to make the fan in my laptop sound like a jet engine
+ */
